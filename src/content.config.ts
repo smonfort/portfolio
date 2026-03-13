6521bcbae98b7ccf7fde5423ce72e577902c8e00
Stéphane <stephane.monfort@gmail.com>
2026-03-13 21:26:18 +0100

    fix: deprecation for zod

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,9 @@
-import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
+import { glob } from "astro/loaders";
 
 const blog = defineCollection({
-  loader: glob({ pattern: '**/*.md', base: './src/content/blog' }),
+  loader: glob({ pattern: "**/*.md", base: "./src/content/blog" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
