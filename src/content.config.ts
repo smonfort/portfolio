ce221a43eea05114d04fe7e1ff3bcfb436cc24f1
Stéphane <stephane.monfort@gmail.com>
2026-03-16 17:49:54 +0100

    chore: astro api deprecation

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,5 @@
 import { defineCollection } from 'astro:content';
-import { z } from 'astro/zod';
+import { z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
