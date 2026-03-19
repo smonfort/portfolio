69d5f2afcf3a8d07ecdfdf6e91a00f243954f10f
Stéphane <stephane.monfort@gmail.com>
2026-03-19 10:28:47 +0100

    feat: add optional slug in frontmatter

--- src/content.config.ts
+++ src/content.config.ts
@@ -9,6 +9,7 @@ const blog = defineCollection({
       title: z.string(),
       description: z.string(),
       date: z.coerce.date(),
+      slug: z.string().optional(),
       tags: z.array(z.string()).max(5).default([]),
       draft: z.boolean().default(false),
       image: image().optional(),
