24b35d7545b6bc18fe3f0b1a58b0ace3f86ab6f2
Stéphane <stephane.monfort@gmail.com>
2026-03-19 20:40:15 +0100

    feat: add likec4

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,20 +6,20 @@ import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 import rehypeExternalLinks from 'rehype-external-links';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.monfort.pro',
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [
-    sitemap({
-      serialize(item) {
-        item.lastmod = new Date().toISOString();
-        return item;
-      },
-    }),
-  ],
+  integrations: [sitemap({
+    serialize(item) {
+      item.lastmod = new Date().toISOString();
+      return item;
+    },
+  }), mdx()],
   markdown: {
     rehypePlugins: [[rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }]],
   },
